b67cc5e3f05ec0cf5f2789cccfa434abf3f14880
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-05-10 07:52:00 -0300

    aplicando react Compiler

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const orderSummary = [
 	{
 		name: 'Notebook Samsung NP550XDA KU1BR',
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import babel from 'vite-plugin-babel';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		babel({
+			babelConfig: {
+				plugins: ['babel-plugin-react-compiler'],
+			},
+		}),
+	],
 });
